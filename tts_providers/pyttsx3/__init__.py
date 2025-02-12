99770baaf877debba49fab3d1ced867ae299a87b
trentw <trentw@users.noreply.github.com>
2025-02-11 22:05:13 -0800

    Cleaning up: - Previous parser - Previous parser utilities - Previous TTS registry - PyTTSX3 provider (dated)

--- tts_providers/pyttsx3/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pyttsx3_tts_provider import Pyttsx3TTSProvider
-from ..registry import register_provider
-
-register_provider('pyttsx3', Pyttsx3TTSProvider)
--- tts_providers/registered_tts_providers.py
+++
@@ -1,7 +0,0 @@
-"""
-Import all available TTS providers.
-New providers should be imported here to be registered with the system.
-"""
-from . import elevenlabs
-from . import pyttsx3
-from . import openai
